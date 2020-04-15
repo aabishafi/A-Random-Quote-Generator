577622159ffa2ade0e3c1af677e0492b7a0a8eb8
aabishafi <adammoha21@gmail.com>
2020-04-16 10:59:46 +1200

    qoutes added

--- js/script.js
+++ js/script.js
@@ -4,30 +4,50 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 // For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
  * `quotes` array 
-***/
-
+ ***/
+var quotes = [{
+        quote: "you'll never find a rainbow if you're looking down",
+        author: 'Charles Chaplin'
+    },
+    {
+        quote: "Customers will never love a company until the employees love it first",
+        author: "Simon Sinek"
+    },
+    {
+        quote: "if you talk it, it's dream, if you envision it, it's possible, but if you schedule it, it's real",
+        author: "tony robbins"
+    },
+    {
+        quote: "Leadership is the ability to hide your panic from others",
+        author: "Lao Tuz"
+    },
+    {
+        quote: "your most unhappy customers are your greatest source of learning",
+        author: "Bill Gates"
+    }
+]
 
 
 /***
  * `getRandomQuote` function
-***/
+ ***/
 
 
 
 /***
  * `printQuote` function
-***/
+ ***/
 
 
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
-***/
+ ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
